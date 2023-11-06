effa48f9fcc3299eda15f2fc1dd40c095205b9bd
jamesxu <xxj8711@gmail.com>
2023-11-06 18:24:37 +0800

    Highter

---
+++ dws.py
@@ -0,0 +1,16 @@
+import sys
+
+class Dws:
+
+    def __init__:
+        pass
+
+
+    # 光耦#1
+    # 高光幕
+
+    # 测量重量
+    # 长宽光幕
+
+    # 光耦#2
+    
---
+++ highter.py
@@ -0,0 +1,12 @@
+import sys
+import ser
+
+
+# 高光幕
+class Highter:
+
+    def __init__:
+        # 串口
+        pass
+
+    def read
