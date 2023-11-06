a4605128b99f8959924c88c2deaa2380865f651c
jamesxu <xxj8711@gmail.com>
2023-11-06 18:30:27 +0800

    highter: hexFormat

--- highter.py
+++ highter.py
@@ -5,8 +5,19 @@ import ser
 # 高光幕
 class Highter:
 
-    def __init__:
+    def __init__(self):
         # 串口
-        pass
+        ser = serial.Serial(portTmp[0], 115200, timeout=0.05)
+        result = data_parse.hexShow(SerialOp.read(1000))
 
-    def read
+    def formatHex(argv):
+        result = ''
+        hLen = len(argv)
+        for i in range(hLen):
+            if isinstance(argv[i], int):
+                hvol = argv[i]
+            else:
+                hvol = ord(argv[i])
+            hhex = '%02x' % hvol
+            result += hhex + ' '
+        return result
