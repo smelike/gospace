09d21e5383fb3367772d205d605ddd2da7ec5e89
jamesxu <xxj8711@gmail.com>
2023-11-06 20:04:18 +0800

    motor & solidworks

---
+++ red_motor.py
@@ -0,0 +1,18 @@
+import sys
+
+class Redmotor:
+
+    def __init__(self):
+        # 串口
+
+
+    # 启动电机
+    def turn_on(self):
+
+    
+    # 关闭电机
+    def turn_off(self):
+
+
+    # 电机状态
+    def run_status(self):
