11be7cf9f01a7e375dfb0622e3b576c848b32656
jamesxu <xxj8711@gmail.com>
2023-11-22 17:33:08 +0800

    L1RZ-1

--- lwter.py
+++ lwter.py
@@ -4,12 +4,26 @@ import time
 # 长宽光幕 L1RZ
 # 型号: ECSL16005L1RZ-1
 # 160 个 LED 
+# 返回数据格式：01 03 08 00 22 00 77 00 56 00 17 C3 C0 
+# 数据段是：00 22 00 77 00 56 00 17
 class Lwter(SerialDeviceBase):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        
+    
+    def get_led_data(self):
+        respBytes = self.read_data()
+        if respBytes and len(respBytes) > 13:
+            # 0 1 2 3 ~ len - 2
+            # heightBytes = respBytes[3:len(respBytes)-2]
+            # print(heightBytes)
+            # data_bytes = heightBytes
+            # 01 03 08 00 00 00 00 00 00 00 00 95 D7
+            # 返回数据：去掉前面3个字节，后面2个字节是CRC校验
+            return [respBytes[i+3:i+11].hex() for i in range(0, len(respBytes), 13)]
+        else:
+            return None
 
 if __name__ == "__main__":
 
@@ -22,9 +36,10 @@ if __name__ == "__main__":
     loop = 0
     while True:
         loop += 1
-        resp = lwter.read_data()
+        # resp = lwter.read_data()
+        resp = lwter.get_led_data()
         if resp:
-            resp_list.append(resp)
+            resp_list += resp
             # print(resp)
         # time.sleep(0.1) 
         # 100 ms * 9 = 900 ms
